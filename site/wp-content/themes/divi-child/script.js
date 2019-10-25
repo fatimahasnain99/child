f7e108a698ec13e2d9fdb02ecaa0caf55d22e6c1
capricornpopcorn <capricornpopcorn@gmail.com>
2019-10-25 21:34:48 +0500

    child

---
+++ site/wp-content/themes/divi-child/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.row').slick({
+    'infinite': 'true',
+    'slidesToShow': 3,
+    'slidesToScroll': 3,
+    'arrows': 'true',
+    'autoplay': 'true',
+  })
+ });
